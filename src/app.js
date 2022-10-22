bb97d8c79a500248aff7254b9c5ecdaf55f7286d
promise <108261992+promiseowa@users.noreply.github.com>
2022-10-22 07:49:27 -0700

    updated app.js

--- src/app.js
+++ src/app.js
@@ -48,7 +48,7 @@ function displayForecast(response) {
                 forecastDay.time
               )}</div>
                 <img
-                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                  src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
                     forecastDay.condition.icon
                   }.png"
                   alt=""
